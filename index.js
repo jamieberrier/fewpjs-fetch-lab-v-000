5a4bc57d6251f04df76bcbe2ac1b636315eb047c
Jamie Berrier <berrier.jamie@gmail.com>
2020-06-05 17:56:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,8 @@ This will not change the behavior of your fetch().
 */
 function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    renderBooks(json)
-  });
+  .then(response => response.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
